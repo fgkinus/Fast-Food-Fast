b032fb3b9c5099f2a8123bbc0d1f5f30fe3afb8b
fgkinus <fgkinus@gmail.com>
2018-08-30 18:47:02 +0300

    Add method to populate food list from a list of food items
    Add a method to poppulate a list with a list of food items and descriptions as defined in the food item template

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,3 +1,4 @@
+// a function to set open tabs
 function openTab(evt, tabName) {
     // variables
     let i, tabcontent, tablinks;
@@ -19,7 +20,45 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
+// set default tab on page load
 function set_active_tab(ID) {
     // set the active tab by selecting it on page load
     document.getElementById(ID).click()
+}
+
+// a list of all food items
+var foodItems = ["burgerFriesSoda", "drumsticks", 'fries', "meatballs", "pie", "pizza", "rools",
+    "sandwich", "vege_burger"];
+
+// load templates to make them visible
+function showContent(destination) {
+    let path  = '../img/';
+
+    //get the template element:
+    let temp = document.getElementsByTagName("template")[0];
+    //get the img element from the template:
+    item = temp.content.querySelector("img");
+
+    //for each item in image folder
+    for (let i=0;i<foodItems.length;i++){
+        // create a new node based on the item
+        let image = document.importNode(item,true);
+        image.src =  path+foodItems[i]+'.jpg';
+        //append item to list
+        add_to_list(destination,image,'item-'+i)
+
+        // document.body.appendChild(image);
+
+    }
+    // let clon = temp.content.cloneNode(true)
+}
+
+// append items to list
+function add_to_list(ListID,item,itemID) {
+    let ul,li;
+    ul  =document.getElementById(ListID);
+    li  = document.createElement('li')
+    li.appendChild(item);
+    li.setAttribute("id",itemID);
+    ul.appendChild(li);
 }
