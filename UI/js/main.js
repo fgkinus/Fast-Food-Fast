9e3d86152c750caa9c7bccc12308b1697cc490f8
fgkinus <fgkinus@gmail.com>
2018-08-30 17:24:02 +0300

    Rename OpenTab function parameters to 'tabName'

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,4 +1,4 @@
-function opentab(evt, cityName) {
+function openTab(evt, tabName) {
     // variables
     let i, tabcontent, tablinks;
 
@@ -9,13 +9,13 @@ function opentab(evt, cityName) {
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName("tab_titles");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
