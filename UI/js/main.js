6ae071a3af9ce2466931413d9a2bc3922cc91cb9
fgkinus <fgkinus@gmail.com>
2018-08-31 12:59:15 +0300

    Add OOP definitions for FoodItem,Order and Order_status

--- UI/js/main.js
+++ UI/js/main.js
@@ -27,33 +27,36 @@ function set_active_tab(ID) {
 }
 
 // a list of all food items
-var foodItems = ["burgerFriesSoda", "drumsticks", 'fries', "meatballs", "pie", "pizza", "rools",
-    "sandwich", "vege_burger"];
+var foodItems = {
+    "burgerFriesSoda": 2000, "drumsticks": 350, 'fries': 200, "meatballs": 350, "pie": 900, "pizza": 1500, "rools": 750,
+    "sandwich": 150, "vege_burger": 250
+};
 
 // load templates to make them visible
 function showContent(destination) {
-    let path  = '../img/';
+    let path = '../img/';
 
     //get the template element:
     let temp = document.querySelector("#food-item");
 
-        // temp.content.querySelector("img");
-
+    // items
+    let items = create_items(foodItems);
+    let keys = Object.keys(items);
     //for each item in image folder
-    for (let i=0;i<foodItems.length;i++){
+    for (let i = 0; i < keys.length; i++) {
         //get the element from the template:
-        let item = document.importNode(temp.content,true);
+        let clone = document.importNode(temp.content, true);
         // create a new node based on the item
-        let image = item.querySelector("img");
-        image.src =  path+foodItems[i]+'.jpg';
+        let image = clone.querySelector("img");
+        image.src = items[keys[i]].image;
 
-        let name= item.querySelector("h5");
-        name.textContent = foodItems[i];
+        let name = clone.querySelector("h5");
+        name.textContent = items[keys[i]].name;
 
-        let cost = item.querySelector("p");
-        // cost.textContent  = "";
+        let cost = clone.querySelector("p");
+        cost.textContent  = items[keys[i]].price;
         //append item to list
-        add_to_list(destination,item,'item-'+i)
+        add_to_list(destination, clone, 'item-' + i)
 
         // document.body.appendChild(image);
 
@@ -62,11 +65,53 @@ function showContent(destination) {
 }
 
 // append items to list
-function add_to_list(ListID,item,itemID) {
-    let ul,li;
-    ul  =document.getElementById(ListID);
-    li  = document.createElement('li')
+function add_to_list(ListID, item, itemID) {
+    let ul, li;
+    ul = document.getElementById(ListID);
+    li = document.createElement('li')
     li.appendChild(item);
-    li.setAttribute("id",itemID);
+    li.setAttribute("id", itemID);
     ul.appendChild(li);
 }
+
+// food object constructor
+function FoodItem(id, name, price, image_url) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.image = image_url;
+}
+
+// Order object constructor
+function Order(id, item_ID, quntity, location) {
+    this.id = id;
+    this.item_id = item_ID;
+    this.quantity = quantity;
+    this.location = location;
+    this.time = Date();
+}
+
+// order status object
+function Order_status(ID, order_id, status) {
+    this.id = ID;
+    this.order_ID = order_id;
+    this.status = staus;
+    this.time = Date();
+}
+
+function create_items(items_list) {
+    let items = [];
+    let keys = Object.keys(items_list);
+    for (let i = 0; i < keys.length; i++) {
+        let name = keys[i];
+        let price = items_list[name];
+        let url = "../img/" + name + ".jpg";
+        let item = new FoodItem(i, name, price, url);
+        items.push(item)
+    }
+    return items;
+}
+
+
+// test function
+console.log(create_items(foodItems));
