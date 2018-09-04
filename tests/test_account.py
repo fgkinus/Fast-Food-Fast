909e620d6e8bf03562c28742f4819930752734a0
fgkinus <fgkinus@gmail.com>
2018-09-04 16:42:01 +0300

    Add unitt tests for Accounts
    Add test cases for accounts methods

---
+++ tests/test_account.py
@@ -0,0 +1,60 @@
+from app.Accounts import Models
+from passlib.hash import pbkdf2_sha256 as sha256
+
+
+class TestAccounts(object):
+    """test cases for User Object"""
+
+    def test_user_create(self):
+        user = Models.User(username='test', firstname='firstname', surname='sir', secondname='second', password='pass',
+                           email='test@test.com')
+        assert user.username == 'test'
+        assert hasattr(user, 'password') is True
+
+    def test_admin_create(self):
+        user = Models.Admin(username='test-admin', firstname='firstname', surname='sir', secondname='second',
+                            password='pass',
+                            email='test@admin.com')
+        assert user.username == 'test-admin'
+        assert hasattr(user, 'password') is True
+        assert hasattr(user, 'email') is True
+
+    def test_password_encryption(self):
+        user = Models.User(username='test', firstname='firstname', surname='sir', secondname='second', password='pass',
+                           email='test@test.com')
+        assert user.password != 'pass'
+
+    def test_password_decryption(self):
+        user = Models.Admin(username='test-admin', firstname='firstname', surname='sir', secondname='second',
+                            password='pass',
+                            email='test@admin.com')
+
+        assert sha256.verify('pass', user.password)
+
+    def test_user_is_not_admin(self):
+        user = Models.User(username='test', firstname='firstname', surname='sir', secondname='second', password='pass',
+                           email='test@test.com')
+
+        assert user.get_admin_status() is False
+
+    def test_admin_is_admin(self):
+        user = Models.Admin(username='test-admin', firstname='firstname', surname='sir', secondname='second',
+                            password='pass',
+                            email='test@admin.com')
+        assert user.get_admin_status() is True
+
+    def test_set_user_id_method(self):
+        number = len(Models.users)
+        user = Models.User(username='test-admin', firstname='firstname', surname='sir', secondname='second',
+                           password='pass',
+                           email='test@admin.com')
+        assert user.ID == number + 1
+        assert len(Models.users) == user.ID
+
+    def test_set_admin_id_method(self):
+        number = len(Models.admin)
+        user = Models.Admin(username='test-admin', firstname='firstname', surname='sir', secondname='second',
+                            password='pass',
+                            email='test@admin.com')
+        assert user.ID == number + 1
+        assert len(Models.admin) == user.ID
