7d042285aa53032f21a6b5a8260673c4387e56c1
fgkinus <fgkinus@gmail.com>
2018-09-04 06:04:59 +0300

    Add app inititalisation logic

---
+++ run.py
@@ -0,0 +1,18 @@
+import os
+
+from app import create_app
+from app.Models import API
+
+# import configuration setting
+config_name = os.getenv('APP_SETTINGS')
+
+# inititlizre the app object
+app = create_app(__name__, config_name)
+
+# initialize the api object
+api = API(app, version='1.0', title='Fast-Food-Fast API',
+          description="A food delivery service application API")
+app = api.app
+
+if __name__ == '__main__':
+    api.app.run()
