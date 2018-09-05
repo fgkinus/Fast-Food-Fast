6ea58eb13312091dd1334f003c7f9079c9a3724a
fgkinus <fgkinus@gmail.com>
2018-09-05 13:51:45 +0300

    Add decorator functions for JWT token authentication and authorisation

---
+++ app/Accounts/decorators.py
@@ -0,0 +1,37 @@
+# Create a function that will be called whenever create_access_token
+from functools import wraps
+
+from flask import jsonify
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_claims
+
+from run import jwt
+
+
+# is used. It will take whatever object is passed into the
+# create_access_token method, and lets us define what custom claims
+# should be added to the access token.
+@jwt.user_claims_loader
+def add_claims_to_access_token(user):
+    return {'admin': user.get_admin_status()}
+
+
+# Create a function that will be called whenever create_access_token
+# is used. It will take whatever object is passed into the
+# create_access_token method, and lets us define what the identity
+# of the access token should be.
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.username
+
+
+def admin_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        claims = get_jwt_claims()
+        if claims['admin'] is False:
+            return jsonify(msg="Admin Users only!!!"), 401
+        else:
+            return fn(*args, **kwargs)
+
+    return wrapper
