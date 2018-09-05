cfef3c664ad36c051196601abc5b31b14fe0d01e
fgkinus <fgkinus@gmail.com>
2018-09-05 19:31:11 +0300

    Remove problemetic JWTmanager class instanciation

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import os
 
-from flask_jwt_extended import JWTManager
-
-from app import create_app
-from app.Models import API
+from app import create_app, jwt
+from app.Models import API, URLS
+from app.urls import urls
 
 # import configuration setting
 config_name = os.getenv('APP_SETTINGS')
@@ -12,13 +11,13 @@ config_name = os.getenv('APP_SETTINGS')
 app = create_app(__name__, config_name)
 app.config['JWT_SECRET_KEY'] = os.getenv('SECRET')
 
-# initialize JWT auth
-jwt = JWTManager(app)
-
 # initialize the api object
-api = API(app, version='1.0', title='Fast-Food-Fast API',
+api = API(app, jwt, version='1.0', title='Fast-Food-Fast API',
           description="A food delivery service application API")
-api.register_namespace()
+
+# register urls
+api = URLS(api, urls).get_api()
+
 app = api.app
 
 if __name__ == '__main__':
