d42cd76a1268f629cb08dfbe86c99d0a64e2299c
fgkinus <fgkinus@gmail.com>
2018-09-12 14:56:12 +0300

    Update get all orders to serialize the items ids

--- app/Orders/Views.py
+++ app/Orders/Views.py
@@ -19,9 +19,15 @@ class ViewMenuOrders(Resource):
 
     @jwt_required
     def get(self):
+        """fetch all orders"""
         orders = Orders().get_all_orders()
+
+        # add items to the serialized output
+        for order in orders:
+            order.item = order.item.ID
+
         order_items = self.schema.dump(orders, many=True)
-        return order_items
+        return jsonify(order_items)
 
     @admin_required
     def post(self):
