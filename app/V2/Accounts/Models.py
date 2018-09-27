0018fc82b240b3b92a16bd1cec440cd8b5c56286
fgkinus <fgkinus@gmail.com>
2018-09-27 15:21:07 +0300

    Add add_user functionality to the User class

--- app/V2/Accounts/Models.py
+++ app/V2/Accounts/Models.py
@@ -1,7 +1,7 @@
 """CLasses with methods to interact with the Database """
-from flask_restplus import Resource
 
 from app.V1.Accounts.Models import User as Base, UserSchema
+from app.V2 import DB
 
 
 class User(Base):
@@ -11,10 +11,22 @@ class User(Base):
         """initialise the class"""
         self.parser = UserSchema()
 
-    def add_user(self,details):
+    def add_user(self, details):
         """
         Add a  user to the DB and return the user
         :param details:
         :return: user
         """
-        user = self.parser.dump(details)
+        details = dict(details)
+        details.update({'isadmin': False})
+        details['password'] = self.generate_hash(details['password'])
+        details = (
+            details['username'],
+            details['first_name'],
+            details['second_name'],
+            details['surname'],
+            details['email'],
+            details['password'],
+            details['isadmin'],
+        )
+        DB.execute_procedures('add_user', details)
