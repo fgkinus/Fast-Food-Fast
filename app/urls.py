4b00ccb750f09385ef8acf98b921a507f64c9ba6
fgkinus <fgkinus@gmail.com>
2018-10-01 10:54:54 +0300

    Add all endpoints on API V2
    Add API v2 dict to URLs file

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from app.V1.Orders.Views import namespace as order_ns
 
 from app.V2.Accounts.Views import namespace as auth_ns_2
 from app.V2.MenuItems.Views import namespace as menu_ns_2
+from app.V2.Orders.Views import namespace as orders_ns_2
 
 # A dictionary of key value pairs of namespace and path
 urls_v1 = {
@@ -15,5 +16,5 @@ urls_v1 = {
 urls_v2 = {
     auth_ns_2: '/api/v2/auth',
     menu_ns_2: '/api/v2/menu',
-    order_ns: '/api/v2/orders'
+    orders_ns_2: '/api/v2/orders'
 }
