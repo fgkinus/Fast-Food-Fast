d91098ceb3895839d821c3273befb63f47a11021
fgkinus <fgkinus@gmail.com>
2018-10-06 15:39:53 +0300

    [Chore #78933385] add hashing to all passwords
    Add hashing to the passwords displayed on signup.

--- app/V2/queries/__init__.py
+++ app/V2/queries/__init__.py
@@ -2,8 +2,14 @@ import os
 
 import psycopg2
 from dotenv import load_dotenv  # import the environment files
+from flask_restplus import abort
+from psycopg2._psycopg import Error
 
 load_dotenv(verbose=True)
 URL = os.getenv('DATABASE_URL')
 queries = os.path.abspath(os.path.dirname(__file__))
-connection = psycopg2.connect(URL)
+
+try:
+    connection = psycopg2.connect(URL)
+except Error as e:
+    raise e
